1711bb547d4bd2a7b49f3495af21cca7ca1e544f
sekhabiruddin <sksabiruddin81@gmail.com>
2024-07-14 16:14:42 +0530

    cros problem

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["https://neodove-frontend.vercel.app", "http://localhost:5173"],
+    origin: ["https://neodove-frontend.vercel.app"],
     credentials: true,
   })
 );
